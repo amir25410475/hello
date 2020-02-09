a99347452d2fa7d1ea49cadb9f139bf7456711c0
Amir <amir25410475@gmail.com>
2020-02-09 14:30:27 +0600

    Updates

---
+++ precache-manifest.e04785f11899e1e5fda191a1cd4b0140.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14abf7b8e62b2e101d696df5cadf6a1c",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "3d129a084d1dd04c0aea",
+    "url": "/hello/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "77e767f6030a4587b85b",
+    "url": "/hello/static/js/2.3358e002.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/hello/static/js/2.3358e002.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d129a084d1dd04c0aea",
+    "url": "/hello/static/js/main.e6a12b32.chunk.js"
+  },
+  {
+    "revision": "ca32f41a501d241e5069",
+    "url": "/hello/static/js/runtime-main.f3735c81.js"
+  }
+]);
